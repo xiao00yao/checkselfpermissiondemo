c741235e2ed14262ca6598bce371d9679347aee1
xiao00yao <1448363422@qq.com>
2020-07-30 16:13:00 +0800

    android 6.0及以上版本权限申请

---
+++ app/src/main/java/com/xy/checkselfpermissiondemo/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.xy.checkselfpermissiondemo
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
+/**
+ * android 6.0及更高版本需要权限申请
+ * 开发环境如下:
+ ** Android studio ->版本 4.0
+ * project的gradle版本 -> distributionUrl=https\://services.gradle.org/distributions/gradle-6.1.1-all.zip
+ */
+class MainActivity : AppCompatActivity() {
+    //需请求的权限  此处只是作为一个示例，请求多个权限的操作，实际操作中打电话不需要 READ_EXTERNAL_STORAGE 权限
+    var permissions = arrayListOf(Manifest.permission.CALL_PHONE,Manifest.permission.READ_EXTERNAL_STORAGE)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //点击事件
+    fun funCall(view: View) {
+        val isHavePermission = cheachPermisssion()
+        if (isHavePermission){
+            call()
+            return
+        }
+        ActivityCompat.requestPermissions(this, permissions.toTypedArray(),100)
+
+    }
+    //检查权限
+    fun  cheachPermisssion():Boolean{
+        /**
+         * PackageManager.PERMISSION_GRANTED
+         * PackageManager.PERMISSION_DENIED
+         */
+        permissions.forEach {
+            if (ContextCompat.checkSelfPermission(this,it)==PackageManager.PERMISSION_DENIED){
+                return false
+            }
+        }
+        return true
+    }
+    //拨打电话
+    fun call(){
+        var intent = Intent(Intent.ACTION_CALL, Uri.parse("tel:" + 12345678945))
+        this.startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode==100){
+            var canCall = true
+            for (i in permissions.indices){
+                if (grantResults[i]==PackageManager.PERMISSION_DENIED){
+                    canCall = false
+                }
+            }
+            if (canCall){ //全部权限都允许
+                call()
+            }else{
+                //此处你也可以添加一个弹窗 让用户选择是否开启权限
+                Toast.makeText(this,"请到设置中打开相应的权限，否侧影响正常操作",Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+    }
+}
